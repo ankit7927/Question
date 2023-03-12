f50a67032adfa7820b2f2ceeb9220c8bc581058d
ankit <code7lancer@gmail.com>
2023-03-12 04:48:54 -0700

    removed hard coding..

--- src/pages/NewQuestion.jsx
+++ src/pages/NewQuestion.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import Loading from '../components/Loading'
 import Caxios from '../extras/Caxios'
 
 const NewQuestion = () => {
@@ -9,71 +8,88 @@ const NewQuestion = () => {
   const [tags, settags] = useState("")
   const [title, settitle] = useState("")
   const [conent, setconent] = useState("")
-  const [loading, setloading] = useState(false)
-  const [message, setmessage] = useState({})
   const token = useSelector((state) => state.user.token)
+
+  const [reqINT, setreqINT] = useState({
+    loading: false,
+    success: false,
+    error: ""
+  })
+
 
   function handleSubmit(e) {
     e.preventDefault()
-    setloading(true)
+    setreqINT({
+      loading: true,
+      success: false,
+      error: ""
+    })
+    if (token === "") {
+      navigate("/signin")
+      return
+    }
     Caxios(token).post(`/ques/new-question`,
       {
         "title": title,
         "content": conent,
-        "email": "email",
         "tags": tags,
       }).then(res => {
-        setloading(false)
-        navigate("/")
+        settags("")
+        settitle("")
+        setconent("")
+        setreqINT({
+          loading: false,
+          success: true,
+          error: ""
+        })
       }).catch(err => {
         console.log(err)
-        setmessage({ content: err.response.data, type: "warning" })
-        setloading(false)
+        setreqINT({
+          loading: false,
+          success: false,
+          error: err.response.data
+        })
       })
   }
 
-  if (loading) {
-    return <Loading />
-  } else {
-    return (
-      <section className="ask-question py-5">
-        <div className="container">
-          {
-            message.conent
-              ? <div className={`alert alert-${message.type}`} role="alert">
-                {message.conent}
-              </div>
-              : <></>
-          }
 
-          <h1 className="text-center mb-4">Ask a Question</h1>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-              <label for="questionTitle" className="form-label">Title</label>
-              <input type="text" className="form-control" id="questionTitle" placeholder="Enter your question title" onChange={(e) => settitle(e.target.value)} />
+  return (
+    <section className="ask-question py-5">
+      <div className="container">
+        {
+          reqINT.error !== ""
+            ? <div className='alert alert-danger' role="alert">
+              {reqINT.error}
             </div>
-            <div className="mb-3">
-              <label for="questionBody" className="form-label">Body</label>
-              <textarea className="form-control" id="questionBody" rows="10" placeholder="Enter your question description" onChange={(e) => setconent(e.target.value)}></textarea>
-            </div>
-            <div className="mb-3">
-              <label for="questionTags" className="form-label">Tags</label>
-              <input type="text" className="form-control" id="questionTags" placeholder="Enter your question tags (separated by commas)" onChange={(e) => settags(e.target.value)} />
-            </div>
-            <button type="submit" className="btn btn-primary">
-              {
-                loading
-                  ? <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-                  : <></>
-              }
-              Submit
-            </button>
-          </form>
-        </div>
-      </section>
-    )
+            : <></>
+        }
 
-  }
+        <h1 className="text-center mb-4">Ask a Question</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label for="questionTitle" className="form-label">Title</label>
+            <input type="text" className="form-control" id="questionTitle" placeholder="Enter your question title" onChange={(e) => settitle(e.target.value)} />
+          </div>
+          <div className="mb-3">
+            <label for="questionBody" className="form-label">Body</label>
+            <textarea className="form-control" id="questionBody" rows="10" placeholder="Enter your question description" onChange={(e) => setconent(e.target.value)}></textarea>
+          </div>
+          <div className="mb-3">
+            <label for="questionTags" className="form-label">Tags</label>
+            <input type="text" className="form-control" id="questionTags" placeholder="Enter your question tags (separated by commas)" onChange={(e) => settags(e.target.value)} />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            {
+              reqINT.loading
+                ? <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                : <></>
+            }
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
+  )
 }
 
 export default NewQuestion
